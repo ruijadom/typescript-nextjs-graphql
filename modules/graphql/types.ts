55a247e6bb2e7521b7395f54bad17dd31a9eb9c3
ruijadom <ruijadomingues@gmail.com>
2022-05-26 22:10:55 +0100

    feat: prisma resolver

--- modules/graphql/types.ts
+++ modules/graphql/types.ts
@@ -5,7 +5,18 @@ export const testQuery = extendType({
   definition: t => {
     t.boolean("test", {
       args: { bool: nonNull(booleanArg()) },
-      resolve: (_, { bool }) => bool,
+      resolve: async (_, { bool }, { prisma }) => {
+        await prisma.user.create({
+          data: {
+            email: "johndoe@gmail.com",
+            username: "johndoe",
+            passhash: "1234",
+          },
+        });
+        const users = await prisma.user.findMany();
+        console.log(users);
+        return bool;
+      },
     });
   },
 });
